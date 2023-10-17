d57625758158c626526ab06a0ac9358905bae69a
Student <Student@205-10>
2023-10-17 15:56:47 +0800

    加入專案檔案。

---
+++ KidsApp/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace KidsApp
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+        }
+    }
+}
